9f61923fdeb221bf70366872fec66b412844a235
Jeremy Bush <contractfrombelow@gmail.com>
2014-02-12 10:38:27 -0600

    Handle year rolling

--- date_calculator.go
+++ date_calculator.go
@@ -1,15 +1,22 @@
 package main
 
 import "time"
+import "errors"
 
-func NextPayDate(start_date time.Time, num_weeks int32) time.Time {
+func NextPayDate(start_date time.Time, num_weeks int32) (time.Time, error) {
+  original_year := start_date.Year()
+
   date := start_date.Add(time.Duration(num_weeks*24*7)*time.Hour)
 
   for (Weekend(date.Weekday()) == true) {
     date = date.Add(-time.Duration(24*time.Hour))
   }
 
-  return date
+  if date.Year() != original_year {
+    return date, errors.New("Cannot iterate past current year")
+  }
+
+  return date, nil
 }
 
 func Weekend(weekday time.Weekday) bool {
--- date_calculator_test.go
+++ date_calculator_test.go
@@ -7,7 +7,7 @@ func TestNextPayDateWithWeekFrequency(t *testing.T) {
   in := time.Date(2014, time.February, 13, 0, 0, 0, 0, time.UTC)
   out := time.Date(2014, time.February, 20, 0, 0, 0, 0, time.UTC)
 
-  if x := NextPayDate(in, 1); x != out {
+  if x, _ := NextPayDate(in, 1); x != out {
     t.Errorf("NextPayDate(%v) = %v, want %v", in, x, out)
   }
 }
@@ -16,7 +16,7 @@ func TestFallsBackToFridayIfPaydayIsOnSaturday(t *testing.T) {
   in := time.Date(2014, time.February, 8, 0, 0, 0, 0, time.UTC)
   out := time.Date(2014, time.February, 21, 0, 0, 0, 0, time.UTC)
 
-  if x := NextPayDate(in, 2); x != out {
+  if x, _ := NextPayDate(in, 2); x != out {
     t.Errorf("NextPayDate(%v) = %v, want %v", in, x, out)
   }
 }
@@ -25,7 +25,15 @@ func TestFallsBackToFridayIfPaydayIsOnSunday(t *testing.T) {
   in := time.Date(2014, time.February, 9, 0, 0, 0, 0, time.UTC)
   out := time.Date(2014, time.February, 21, 0, 0, 0, 0, time.UTC)
 
-  if x := NextPayDate(in, 2); x != out {
+  if x, _ := NextPayDate(in, 2); x != out {
     t.Errorf("NextPayDate(%v) = %v, want %v", in, x, out)
+  }
+}
+
+func TestErrorWhenRollsToNextYear(t *testing.T) {
+  in := time.Date(2014, time.December, 26, 0, 0, 0, 0, time.UTC)
+
+  if _, err := NextPayDate(in, 2); err == nil {
+    t.Errorf("NextPayDate(%v) = %v, want not %v", in, err, nil)
   }
 }
